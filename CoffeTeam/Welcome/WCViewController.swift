c660566bd0a135d458176e4d27f4f220fc988d87
Alexmg86 <alexmg86@mail.ru>
2020-03-11 17:01:55 +0300

    welcome icons, fonts, design

---
+++ CoffeTeam/Welcome/WCViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WCViewController.swift
+//  CoffeTeam
+//
+//  Created by Алексей Морозов on 11.03.2020.
+//  Copyright © 2020 Алексей Морозов. All rights reserved.
+//
+
+import UIKit
+
+class WCViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var mainTitle: UILabel!
+    @IBOutlet weak var subTitle: UILabel!
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var createAccount: UIButton!
+    @IBOutlet weak var enterAccountBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

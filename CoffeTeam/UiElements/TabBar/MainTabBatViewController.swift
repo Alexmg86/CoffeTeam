e025a58945c15c7d4b847b40d6e2d5cf266de8dc
Alex <alexmg86@mail.ru>
2020-04-05 23:47:50 +0300

    Набросок tabBar

---
+++ CoffeTeam/UiElements/TabBar/MainTabBatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBatViewController.swift
+//  CoffeTeam
+//
+//  Created by Алексей Морозов on 05.04.2020.
+//  Copyright © 2020 Алексей Морозов. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBatViewController: BubbleTabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tabBar.tintColor = #colorLiteral(red: 0.8509803922, green: 0.6196078431, blue: 0.5882352941, alpha: 1)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

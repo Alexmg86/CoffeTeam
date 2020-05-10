b90419c162b344ca37d5fe4c580dead407dcce33
Alex <alexmg86@mail.ru>
2020-05-10 17:16:11 +0300

    Набросок групп

---
+++ CoffeTeam/Group/GroupAddViewController.swift
@@ -0,0 +1,33 @@
+//
+//  GroupAddViewController.swift
+//  CoffeTeam
+//
+//  Created by Алексей Морозов on 10.05.2020.
+//  Copyright © 2020 Алексей Морозов. All rights reserved.
+//
+
+import UIKit
+
+class GroupAddViewController: KeyboadController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closeButton(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

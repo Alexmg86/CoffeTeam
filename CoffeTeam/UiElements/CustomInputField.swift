acb3723d7fadbae7c038defdf1ff169c5c73431f
Alex <alexmg86@mail.ru>
2020-05-24 15:31:12 +0300

    переделан CustomInputField

--- CoffeTeam/UiElements/CustomInputField.swift
+++ CoffeTeam/UiElements/CustomInputField.swift
@@ -10,30 +10,50 @@ import UIKit
 
 class CustomInputField: UITextField, UITextFieldDelegate {
     
+    var error: Bool = false
+    var count: Int = 0
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+        addSublayer()
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
+        self.delegate = self
         textFieldSetup()
     }
     
-    func textFieldDidChangeSelection(_ textField: UITextField) {
-        textFieldSetup()
+    func addSublayer() {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: frame.height - 1, width: bounds.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1).cgColor
+        bottomLine.name = "custom_underline"
+        self.borderStyle = UITextField.BorderStyle.none
+        self.layer.addSublayer(bottomLine)
     }
-    
-    func textFieldDidBeginEditing(_ textField: UITextField) {
-        print("dd")
-        print(textField.keyboardType)
+
+    func textFieldError() {
+        error = true
+        textFieldSetup()
     }
 
     private func textFieldSetup() {
-        let bottomLine = CALayer()
-        bottomLine.frame = CGRect(x: 0.0, y: frame.height - 1, width: bounds.width, height: 1.0)
         var color = UIColor(red: 230/255, green: 230/255, blue: 230/255, alpha: 1)
         if (self.text!.count > 0) {
             color = UIColor(red: 88/255, green: 150/255, blue: 139/255, alpha: 1)
         }
-        bottomLine.backgroundColor = color.cgColor
-        self.borderStyle = UITextField.BorderStyle.none
-        self.layer.addSublayer(bottomLine)
+        if (count != self.text!.count) {
+            error = false
+        }
+        if (error) {
+            color = UIColor.red
+        }
+        if let sublayers = self.layer.sublayers {
+            if let lay = sublayers.filter({ $0.name == "custom_underline" }).first {
+                lay.backgroundColor = color.cgColor
+            }
+        }
+        count = self.text!.count
     }
-    
 }

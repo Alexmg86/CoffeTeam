15bbc55c0bef8efe2611026e89f98233efec192a
Alex <alexmg86@mail.ru>
2020-05-28 18:10:16 +0300

    Иконки для товаров и shopController

--- CoffeTeam/Shop/ShopAddViewController.swift
+++ CoffeTeam/Shop/ShopAddViewController.swift
@@ -11,26 +11,33 @@ import Alamofire
 import SwiftyJSON
 
 class ShopAddViewController: UIViewController {
-    
-    @IBOutlet weak var pickerTextField : CustomInputField!
-    
+
+    @IBOutlet weak var group_idTextField : CustomInputField!
+    @IBOutlet weak var nameTextField : CustomInputField!
+    @IBOutlet weak var priceTextField : CustomInputField!
+
+    @IBOutlet weak var group_id: UILabel!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var price: UILabel!
+
+    @IBOutlet weak var saveGood: CustomButton!
+
     var pickerOficinas: CustomPickerInput!
-    
     var salutations: JSON = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.loadGroups()
     }
-    
+
     @IBAction func closeButton(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     func loadGroups() {
         AF.request("https://ineedapp:8890/group").responseJSON { [weak self] (response) in
             switch response.result {
@@ -43,12 +50,11 @@ class ShopAddViewController: UIViewController {
             }
         }
     }
-    
-    @IBAction func getData(_ sender: Any) {
+
+    @IBAction func saveGoodTapped(_ sender: UIButton) {
         print(pickerTextField.text as Any)
         print(salutations[pickerTextField.tag])
     }
-
 }
 
 extension UITextField {

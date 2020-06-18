09b0b9e4004bef73fbe96463fafd9fad6d8392b7
Alexmg86 <alexmg86@mail.ru>
2020-06-18 15:59:01 +0300

    Начало таблы

--- CoffeTeam/Group/GroupInnerViewController.swift
+++ CoffeTeam/Group/GroupInnerViewController.swift
@@ -7,29 +7,38 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
-class GroupInnerViewController: UIViewController {
-    
+class GroupInnerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
     var groupName: String = ""
     var groupCode: String = ""
     @IBOutlet weak var code: UILabel!
     @IBOutlet weak var usersList: UITableView!
+    var items = [JSON]()
+    let user = User()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.usersList.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: usersList.frame.size.width, height: 1))
         self.title = groupName
         code.text = groupCode
+        self.usersList.delegate = self
+        self.usersList.dataSource = self
+//        loadItems()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return items.isEmpty ? 0 : items.count
     }
-    */
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.isEmpty ? 0 : items[section]["users"].count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 
 }

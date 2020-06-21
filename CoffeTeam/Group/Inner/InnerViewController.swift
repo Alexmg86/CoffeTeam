67fca35f1d79f7a9866c8d273a4a828c7f71e7e9
Alex <alexmg86@mail.ru>
2020-06-21 19:18:45 +0300

    Иконка

---
+++ CoffeTeam/Group/Inner/InnerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InnerViewController.swift
+//  CoffeTeam
+//
+//  Created by Алексей Морозов on 21.06.2020.
+//  Copyright © 2020 Алексей Морозов. All rights reserved.
+//
+
+import UIKit
+
+class InnerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

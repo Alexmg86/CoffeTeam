456e169a018652b86c99aad1c2fd758546e586db
Alex <alexmg86@mail.ru>
2020-06-21 17:37:52 +0300

    Вынесены пользователи в отдельный контейнер

--- CoffeTeam/Group/GroupInnerViewController.swift
+++ CoffeTeam/Group/GroupInnerViewController.swift
@@ -10,26 +10,41 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class GroupInnerViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class GroupInnerViewController: UIViewController {
 
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    @IBOutlet weak var code: UILabel!
+    @IBOutlet weak var userListView: UIView!
+    
     var groupName: String = ""
     var groupCode: String = ""
     var groupId: String = ""
-    @IBOutlet weak var code: UILabel!
-    @IBOutlet weak var usersList: UITableView!
     var items = [JSON]()
-    var selectedItem: JSON = []
+    var stats = [JSON]()
     let user = User()
     let modelName = "group"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.usersList.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: usersList.frame.size.width, height: 1))
         self.title = groupName
         code.text = groupCode
-        self.usersList.delegate = self
-        self.usersList.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         loadItems()
+    }
+    @IBAction func toggleView(_ sender: Any) {
+        switch segmentControl.selectedSegmentIndex {
+        case 0:
+            userListView.isHidden = false
+            break
+        case 1:
+            userListView.isHidden = true
+            break
+        default:
+            break
+        }
     }
     
     func loadItems() {
@@ -60,35 +75,10 @@ class GroupInnerViewController: UIViewController, UITableViewDelegate, UITableVi
     
     func checkItems(value: Any) {
         let json = JSON(value as Any)
-        items = json.arrayValue
-        usersList.reloadData()
+        items = json["users"].arrayValue
+        stats = json["users"].arrayValue
+        let innerVC = self.children[0] as! InnerTableViewController
+        innerVC.items = items
+        innerVC.tableView.reloadData()
     }
-    
-    func getSelectedItem(indexPath: IndexPath, relation: String) {
-        let itemSection = items[indexPath.section]
-        selectedItem = itemSection[relation][indexPath.row]
-    }
-    
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return items.isEmpty ? 0 : items.count
-    }
-
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.isEmpty ? 0 : items[section]["users"].count
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = usersList.dequeueReusableCell(withIdentifier: "usersListCell", for: indexPath) as! GroupInnerTableViewCell
-        getSelectedItem(indexPath: indexPath, relation: "users")
-        cell.nameLabel.text = selectedItem["name"].string
-        cell.contactLabel.text = selectedItem["email"].string
-        let total = selectedItem["total"].int!
-        cell.countLabel.text = String(total)
-        if total >= 0 {
-            cell.countLabel.textColor = #colorLiteral(red: 0.3450980392, green: 0.5882352941, blue: 0.5450980392, alpha: 1)
-        }
-        cell.accessoryType = .disclosureIndicator
-        return cell
-    }
-
 }
--- CoffeTeam/Group/Inner/InnerTableViewController.swift
+++ CoffeTeam/Group/Inner/InnerTableViewController.swift
@@ -7,84 +7,44 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class InnerTableViewController: UITableViewController {
-
+    
+    var items = [JSON]()
+    var selectedItem: JSON = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.tableFooterView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 1))
+    }
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    func getSelectedItem(indexPath: IndexPath, relation: String) {
+        let itemSection = items[indexPath.section]
+        selectedItem = itemSection[relation][indexPath.row]
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return items.isEmpty ? 0 : items.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return items.isEmpty ? 0 : items[section]["users"].count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "usersListCell", for: indexPath) as! GroupInnerTableViewCell
+        getSelectedItem(indexPath: indexPath, relation: "users")
+        cell.nameLabel.text = selectedItem["name"].string
+        cell.contactLabel.text = selectedItem["email"].string
+        let total = selectedItem["total"].int!
+        cell.countLabel.text = String(total)
+        if total >= 0 {
+            cell.countLabel.textColor = #colorLiteral(red: 0.3450980392, green: 0.5882352941, blue: 0.5450980392, alpha: 1)
+        }
+        cell.accessoryType = .disclosureIndicator
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
